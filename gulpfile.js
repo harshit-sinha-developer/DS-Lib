94e3bc557f566b28c46c8e0f33c29748e7c2fe8f
Harshit Sinha <harshit.sinha@comprotechnologies.com>
2017-02-07 11:42:47 +0530

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,26 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var minify = require('gulp-minify-css');
-
+var browserSync = require('browser-sync');
 gulp.task('js', function(){
     gulp.src('src/scripts/*.js')
     .pipe(concat('script.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('build/scripts/'));
+    .pipe(gulp.dest('build/scripts/'))
+    .pipe(browserSync.reload({
+        stream: true
+    }));
 });
 
-gulp.task('default', ['js'], function(){});
+gulp.task('browserSync', function() {
+   browserSync.init({
+      server: {
+         baseDir: 'build'
+      },
+   });
+});
+gulp.task('default', ['js'], function(){
+    gulp.watch('src/scripts/*.js', function(){
+        gulp.run('js');
+    })
+});
